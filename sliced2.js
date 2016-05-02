5968cbb6234f3bb67b53a34bf2d70edf7a95deb4
PeiDa Kuo <guo.peida@gmail.com>
2016-05-02 11:16:17 -0700

    Used 100(data-bound) divs not collected in rows to represent sliced picture

---
+++ sliced2.js
@@ -0,0 +1,55 @@
+(function () {
+
+  document.onreadystatechange = function () {
+    if (document.readyState == "complete") {
+      console.log("ready");
+
+      var imageSource = "http://cdn2.justdogbreeds.com/justdogbreeds-cdn/photos/plog-content/thumbs/dog-breeds/golden-retriever/large/2955-golden-retriever-75.jpg";
+
+      var image = new Image();
+      image.src = imageSource;
+      var width = image.width, height = image.height;
+
+      var tileWidth = Math.floor(width/10);
+      var tileHeight = Math.floor(height/10);
+
+      d3.select(".image").style({width: width+"px"});
+      // var row = d3.select('.image')
+      // .selectAll(".row")
+      // .data(d3.range(10).map(function (d) {return d; }))
+      // .enter().append("div").classed("row", true)
+      // .style({ height: tileHeight+"px" } );
+
+      var tile = d3.select(".image").selectAll(".tile")
+        .data(d3.range(100).map(function (d) {
+          return { row: Math.floor(d/10), col: d%10, id: d }; }))
+        .enter().append("div").classed("tile", true)
+        .style({ height: tileHeight+"px", width: tileWidth+"px",
+            backgroundPosition: function (d) {
+              // debugger
+              return this.style.backgroundPosition = -d.col*tileWidth+"px "+-d.row*tileHeight+"px";
+          }});
+
+      var drag = d3.behavior.drag().origin(function (d) {
+        return {x: this.offsetLeft, y:this.offsetTop };
+      });
+      tile.call(drag);
+      tile.style({ position: "relative" });
+      drag.on("drag", function (d) {
+
+        if (this.style.left === "") {
+          this.style.left = d3.event.dx+"px";
+        } else {
+          this.style.left = parseInt(this.style.left)+ d3.event.dx +"px";
+        }
+
+        if (this.style.top === "") {
+          this.style.top = d3.event.dy+"px";
+        } else {
+          this.style.top = parseInt(this.style.top)+ d3.event.dy +"px";
+        }
+      });
+      // debugger
+    }
+  }
+})();
