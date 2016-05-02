7fa2dc374b54f228ec5f21eb7e7ec9e1ec4aa31d
PeiDa Kuo <guo.peida@gmail.com>
2016-05-01 22:44:17 -0700

    made a sliced up image with CSS sprite with draggable tiles

--- sliced.js
+++ sliced.js
@@ -1,31 +1,52 @@
 (function () {
+
   document.onreadystatechange = function () {
     if (document.readyState == "complete") {
       console.log("ready");
-      var drag = d3.behavior.drag();
-      var selection = d3.select(".image").data([{ x: 0, y: 0 }]);
 
-      selection.call(drag);
-      // selection.attr("cx", 0).attr("cy", 0);
+      var imageSource = "http://cdn2.justdogbreeds.com/justdogbreeds-cdn/photos/plog-content/thumbs/dog-breeds/golden-retriever/large/2955-golden-retriever-75.jpg";
 
-      selection.style({ position: "absolute" });
-      // drag.origin();
+      var image = new Image();
+      image.src = imageSource;
+      var width = image.width, height = image.height;
 
-      drag.on("drag", function () {
-        // var pos = $('.image').position();
-        // debugger
-        selection.style({
-          top: function (d) {
-            d.y += d3.event.dy;
-            return d.y+"px";
-          },
-          left: function (d) {
-            d.x += d3.event.dx;
-            return d.x+"px";
-          }
-        });
-      }).bind(selection);
-      // debugger
+      var drag = d3.behavior.drag().origin(function (d) {
+        return {x: this.offsetLeft, y:this.offsetTop };
+      });
+      var tileWidth = Math.floor(width/10);
+      var tileHeight = Math.floor(height/10);
+
+      var row = d3.select('.image')
+      .selectAll(".row")
+      .data(d3.range(10).map(function (d) {return d; }))
+      .enter().append("div").classed("row", true)
+      .style({ height: tileHeight+"px" } );
+
+      var tile = row.selectAll(".tile")
+        .data(d3.range(10).map(function (d) { return d; }))
+        .enter().append("div").classed("tile", true)
+        .style({ height: tileHeight+"px", width: tileWidth+"px",
+            backgroundPosition: function (col, i, row) {
+              return this.style.backgroundPosition = -col*tileWidth+"px "+-row*tileHeight+"px";
+          }});
+
+      tile.call(drag);
+      tile.style({ position: "relative" });
+      drag.on("drag", function (d) {
+
+        if (this.style.left === "") {
+          this.style.left = d3.event.dx+"px";
+        } else {
+          this.style.left = parseInt(this.style.left)+ d3.event.dx +"px";
+        }
+
+        if (this.style.top === "") {
+          this.style.top = d3.event.dy+"px";
+        } else {
+          this.style.top = parseInt(this.style.top)+ d3.event.dy +"px";
+        }
+      });
+
     }
   }
 })();
