c06ae14d663f06e8cd91bffb030201eb5687f388
PeiDa Kuo <guo.peida@gmail.com>
2016-05-04 14:05:17 -0700

    fixed a bug that prevents proper sorting (unscramble) in Chrome

--- tiles.js
+++ tiles.js
@@ -18,7 +18,7 @@
 
     unscrambleTiles: function () {
       this.tiles[0].sort(function (a,b) {
-        return a.__data__.id > b.__data__.id
+        return a.__data__.id - b.__data__.id
       }).forEach(function (el) {
         return d3.select(".image").append(function () {
           return el;
