20161a0dae7ff127d48c17d69f4e8a944781b044
PeiDa Kuo <guo.peida@gmail.com>
2016-05-06 18:09:55 -0700

    20x20 grid; completed glitchy slowlyUnscramble(); default is unscrambleTiles()

--- tiles.js
+++ tiles.js
@@ -9,9 +9,12 @@
 
   tiles.prototype = {
     scrambleTiles: function () {
-      d3.shuffle(this.tiles[0]).forEach(function (el) {
-        return d3.select(".image").append(function () {
-          return el;
+      d3.shuffle(this.tiles[0]);
+
+      this.tiles.each(function () {
+        var tile = this;
+        d3.select(".image").append(function () {
+          return tile;
         });
       });
     },
@@ -27,12 +30,29 @@
       this.slowlyReturnToShape();
     },
 
+    slowlyUnscramble: function () {
+      var collection = this.tiles;
+      collection.transition().tween("sort", function (d,i) {
+        var itpl = d3.interpolateRound(d.id-2, d.id);
+        return function (t) {
+          // debugger
+          this.parentNode.insertBefore(this, collection.filter(function (datum2, i) {
+            return datum2.id === itpl(t)+1;
+          }).node());
+        }
+      }).duration(20000);
+
+      this.slowlyReturnToShape();
+    },
+
     slowlyReturnToShape: function () {
+      // if it takes 20s to slowlyUnscramble, slowlyReturnToShape needs a 20s delay
       this.tiles.transition().style({
         top: 0+"px",
         left: 0+"px"
       })
-      .duration(3000);
+      .duration(2000)
+      // .delay(20000);
     },
 
     returnToShape: function () {
