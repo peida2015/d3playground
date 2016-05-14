2b2a939e7618adb200eb857899587a89cfcfb170
PeiDa Kuo <guo.peida@gmail.com>
2016-05-14 16:28:15 -0700

    refactor iframeResize() so it's called upon entering index.html

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 ;(function () {
   document.onreadystatechange = function() {
     if (document.readyState == "complete") {
-      var toggleMenu = function (e) {
+      function iframeResize(e = null) {
         var frame = document.querySelector('iframe');
-        frame.src = e.target.dataset.url;
+
+        frame.src = e === null ? './pic_scrambler.html' : e.target.dataset.url;
 
         setTimeout(function () {
           if (frame.contentDocument.readyState ==='complete') {
             frame.contentDocument.body.scrollHeight < 480 ? frame.height = 510 : frame.height = frame.contentDocument.body.scrollHeight+30;
           }
         }, 250)
-
+      };
+      var toggleMenu = function (e) {
+        iframeResize(e);
         if (e.target !== e.currentTarget) {
           var tabs = e.currentTarget.children;
           for (var i = 0; i < tabs.length; i++) {
@@ -20,9 +23,10 @@
         }
       };
 
+
       var tabs = document.querySelector('.tabs-holder')
       tabs.addEventListener('click', toggleMenu, true);
-
+      iframeResize();
     }
   }
 })();
