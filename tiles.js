f1f52938e8fb7499a54fda73d76c505e8b4aa320
PeiDa Kuo <guo.peida@gmail.com>
2016-11-12 17:43:24 -0800

    slowlyUnscramble now animates quicksort algorithm

--- tiles.js
+++ tiles.js
@@ -8,19 +8,93 @@
     this.tiles = tiles;
     this.tileWidth = tileWidth;
     this.tileHeight = tileHeight;
-  }
+    var that = this;
 
-  tiles.prototype = {
-    scrambleTiles: function () {
+    // private
+    var moveToSpot = function (tile, idx) {
+      d3.select(this)
+      .transition().style({
+        top: (Math.floor(idx/15)-tile.row)*(that.tileHeight+1)+"px",
+        left: (idx%15-tile.col)*(that.tileWidth+1)+"px"
+      })
+      .duration(2000);
+    };
+
+    // public but not in prototype
+    this.scrambleTiles = function () {
       d3.shuffle(this.tiles[0]);
 
       var that = this;
-      this.tiles.each(function (item, idx) {
-        this.style.top = (Math.floor(idx/15)-item.row)*(that.tileHeight+1)+"px";
-        this.style.left = (idx%15-item.col)*(that.tileWidth+1)+"px";
-      });
-    },
+      this.tiles.each(moveToSpot);
+    };
 
+    this.slowlyUnscramble = function (left, right) {
+      if (left === undefined) { var left = 0; }
+      if (right === undefined) { var right = this.tiles[0].length-1; }
+      if (right - left <= 0) { return; }
+
+      var stIdx = left, endIdx = right;
+
+      var that = this;
+
+      // use the first tile as the pivot and assign it ".pivot"
+      var pivot = d3.select(that.tiles[0][left]);
+      pivot.classed("pivot", true);
+
+      // left is the current index of pivot, which should tightly follow left.
+      var pivotId = pivot.datum().id;
+
+      while (right > left) { /* stops when right === left */
+        var nextTile = that.tiles[0][left+1];
+        var leftId = nextTile.__data__.id;
+        if (leftId < pivotId) {
+          var a = pivot.node();
+          var b = nextTile;
+
+          // swap the displayed position by changing top and left values in style properties
+          moveToSpot.call(b, b.__data__, left);
+          moveToSpot.call(a, a.__data__, left+1);
+
+          // concurrent swap within d3 array of nodes
+          that.tiles[0][left] = that.tiles[0][left+1];
+          that.tiles[0][left+1] = pivot.node();
+
+          left++;
+
+        } else {
+          var a = nextTile;
+          var b = that.tiles[0][right];
+
+          // swap the displayed position by changing top and left values in style properties
+          moveToSpot.call(b, b.__data__, left+1);
+          moveToSpot.call(a, a.__data__, right);
+
+          // concurrent swap within d3 array of nodes
+          var temp = that.tiles[0][right];
+          that.tiles[0][right] = that.tiles[0][left+1];
+          that.tiles[0][left+1] = temp;
+
+          if (right > left) { right--; };
+
+        };
+      };
+
+      setTimeout(function () {
+        that.slowlyUnscramble(left+1, endIdx);
+        setTimeout(function () {
+          that.slowlyUnscramble(stIdx, left-1);
+          setTimeout(function () {
+            pivot.classed('pivot', false);
+          }, 300);
+        }, 300);
+      }, 300);
+
+
+
+    };
+  };
+
+  tiles.prototype = {
     unscrambleTiles: function () {
       this.tiles[0].sort(function (a,b) {
         return a.__data__.id - b.__data__.id
@@ -32,29 +106,6 @@
       this.slowlyReturnToShape();
     },
 
-    slowlyUnscramble: function () {
-      var collection = this.tiles;
-
-      // Return to rectangular shape first (2s)
-      this.slowlyReturnToShape();
-
-      // Delay 2s to let slowlyReturnToShape() finish first
-      collection.transition().tween("sort", function (d,i) {
-        var itpl = d3.interpolateRound(d.id-5, d.id);
-        return function (t) {
-          if (d.id === i) { return; }
-
-          this.parentNode.insertBefore(this, collection.filter(function (datum2, i) {
-            var val = itpl(t);
-
-            if (val === d.id) { d.inOrder = true; }
-            return datum2.id === val+1;
-          }).node());
-        }
-      }).ease("elastic")
-      .delay(2000)
-      .duration(5000);
-    },
 
     slowlyUnscramble2: function () {
       // Implementation of the "Grab and Append" method
@@ -113,7 +164,7 @@
               top: 0+"px",
               left: 0+"px"
             })
-            .duration(2000)
+            .duration(200)
       // .delay(20000);
     },
 
